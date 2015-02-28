ede8b5ad5f22cd173f130c131e61fd60c71da7e2
Michael Dorin <mike@Michaels-Mac-mini.local>
2015-02-27 18:10:44 -0600

    Fixed a bad path.

--- startup.sh
+++ startup.sh
@@ -2,11 +2,11 @@
 sleep 10
 sudo modprobe w1-gpio
 sudo modprobe w1-therm
-# sudo /home/pi/tempserver/adb forward tcp:50001 tcp:50001
+# sudo /home/pi/optimized-SMS-pi/adb forward tcp:50001 tcp:50001
 sleep 3
 logger "one to client"
-sudo python /home/pi/tempserver/client.py &
+sudo python /home/pi/optimized-SMS-pi/client.py &
 sleep 1
 logger "one to temp server"
-sudo python /home/pi/tempserver/tempserver.py -p54320 &
+sudo python /home/pi/optimized-SMS-pi/optimized-SMS-pi.py -p54320 &
 
