73cf8459dfd148ac239946695020d994d0bc010e
Michael Dorin <mike@Michaels-Mac-mini.local>
2015-02-28 13:14:58 -0600

    Fix reference problem with read_temp_raw()

--- tempserver.py
+++ tempserver.py
@@ -90,7 +90,7 @@ class TempService (coapy.link.LinkValue):
         lines = val.read_temp_raw()
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
-            lines = read_temp_raw()
+            lines = val.read_temp_raw()
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
             temp_string = lines[1][equals_pos+2:]
